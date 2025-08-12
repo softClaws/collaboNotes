164e3dfa166df5b7f76c21fec92c2444b10af8b9
Abdulkadir <uabdulkadir06@gmail.com>
2025-08-12 20:30:34 +0100

    changed url

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,4 +1,6 @@
 const allowedOrigins =[
-    'https://collabo-notes-app.vercel.app'
+    'https://collabo-notes-app.vercel.app',
+    'https://collabonote.onrender.com',
+    'http://localhost:3000'
 ]
 module.exports = allowedOrigins
