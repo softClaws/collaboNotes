f1ec2ce30e0d3a35f2b7b671b6bb44b43639a15c
Abdulkadir <uabdulkadir06@gmail.com>
2025-08-12 19:48:28 +0100

    corrected main.jsx mistake

--- collabo_note/src/main.jsx
+++ collabo_note/src/main.jsx
@@ -3,15 +3,15 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import { Store } from './app/Store.jsx'
+import { store } from './app/Store.jsx'
 import { Provider } from 'react-redux'
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
-if(process.env.NODE_ENV === 'production') disableReactDevTools
+if(process.env.NODE_ENV === 'production') disableReactDevTools()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store ={Store}>
+    <Provider store ={store}>
         <BrowserRouter>
 
         <Routes>
