9ca5623ffa756c83d856ecc797b58f45a0556ab9
Abdulkadir <uabdulkadir06@gmail.com>
2025-09-16 20:05:20 +0100

    change the s in store.jsx from capital letter to small letter

--- collabo_note/src/features/auth/Prefetch.jsx
+++ collabo_note/src/features/auth/Prefetch.jsx
@@ -1,4 +1,4 @@
-import {store} from '../../app/Store'
+import {store} from '../../app/store'
 import { notesApiSlice } from '../notes/NoteApiSlice'
 import { usersApiSlice } from '../users/UsersApiSlice'
 import { useEffect } from 'react'
